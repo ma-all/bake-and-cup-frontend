115a9b021881a391ce2230354b3976a73a7660db
ma-all <marwaalbaloushi21@gmail.com>
2026-08-08 21:39:25 +0300

    added the add to cart and view details buttons

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -5,13 +5,15 @@ const Menu = (props) => {
         <div className="menu-container">
             {props.menuItems.map((item) => (
                 <div key={item._id} className="menu-card">
-                    <img src={item.img} alt={item.name} className="menu-img" />
-                    <h2>
-                        <Link to={`/menu-items/${item._id}`}>
-                            {item.name}
-                        </Link>
-                        <p>{item.price}</p>
-                    </h2>
+                    <div className="img-container">
+                        <img src={item.img} alt={item.name} className="menu-img" />
+                    </div>
+                    <Link to={`/menu-items/${item._id}`}>
+                        <h2>{item.name}</h2>
+                    </Link>
+                    <h3>Price: {item.price} BHD</h3>
+                    <button>View Details</button>
+                    <button>Add To Cart</button>
                 </div>
             ))}
         </div>
