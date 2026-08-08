a3eaa0f4dd8b7d5b563cc4032bf50979197b4e65
ma-all <marwaalbaloushi21@gmail.com>
2026-08-08 16:44:05 +0300

    added some css to the menu page

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -2,18 +2,19 @@ import { Link } from "react-router"
 
 const Menu = (props) => {
     return (
-        <>
+        <div className="menu-container">
             {props.menuItems.map((item) => (
-                <div key={item._id}>
-                    <h4>
+                <div key={item._id} className="menu-card">
+                    <img src={item.img} alt={item.name} className="menu-img" />
+                    <h2>
                         <Link to={`/menu-items/${item._id}`}>
                             {item.name}
                         </Link>
                         <p>{item.price}</p>
-                    </h4>
+                    </h2>
                 </div>
             ))}
-        </>
+        </div>
     )
 }
 
