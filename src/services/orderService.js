a5d700e5c8f3f32d476d0b368e293252b845079b
Rawan-haji <rawanjan02@gmail.com>
2026-08-09 12:56:38 +0300

    create the service order cart

---
+++ src/services/orderService.js
@@ -0,0 +1,86 @@
+import { data } from "react-router";
+
+const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/orders`;
+
+const index = async () => {
+    const res = await fetch(BASE_URL)
+    const data = await res.json()
+
+    if (!res.ok) {
+        throw new Error(`${res.status}: {data.message}`)
+    }
+
+    return data
+}
+
+const show = async (orderId) => {
+    const res = await fetch(`${BASE_URL}/${orderId}`)
+    const data = await res.json()
+
+    if (!res.ok) {
+        throw new Error(`${res.status}: {data.message}`)
+
+    }
+    return data
+}
+
+
+
+const create = async (formData) => {
+    const res = await fetch(BASE_URL, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+
+        },
+        body: JSON.stringify(formData)
+    })
+
+    const data = await res.json()
+    if (!res.ok) {
+        throw new Error(`${res.status}: {data.message}`)
+    }
+
+    return data
+}
+
+const update = async (orderId, formData) => {
+    const res = await fetch(`${BASE_URL}/${orderId}`, {
+        method: 'PUT',
+        headers: {
+            'Content-type': 'application/json',
+        },
+
+        body: JSON.stringify(formData),
+    })
+    const data = await res.json()
+    if (!res.ok) {
+        throw new Error(`${res.status}: {data.message}`)
+    }
+
+    return data
+
+
+}
+
+const deleteOrder = async (orderId) => {
+    const res = await fetch(`${BASE_URL}/${orderId}`, {
+        method: 'DELETE'
+    })
+
+    const data = await res.json()
+
+    if (!res.ok) {
+        throw new Error(`${res.status}: {data.message}`)
+    }
+
+    return data
+
+}
+export {
+    index,
+    show,
+    update,
+    create,
+    deleteOrder
+}
