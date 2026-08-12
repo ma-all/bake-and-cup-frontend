ead4165b28f4da25b6fd9c50927145d16ae7eb03
Rawan-haji <rawanjan02@gmail.com>
2026-08-12 12:51:37 +0300

    add the try

--- src/services/menuItems.js
+++ src/services/menuItems.js
@@ -1,6 +1,7 @@
 const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/menu-items`;
 
 const index = async () => {
+    try{
     const res = await fetch(BASE_URL, {
         headers: {
             Authorization: `Bearer ${localStorage.getItem('token')}`,
@@ -11,7 +12,10 @@ const index = async () => {
         throw new Error(`${res.status}: ${data.message}`)
     }
     return data
-}
+} catch(error){
+    console.log('Error fetching menu item', error)
+
+}}
 
 export {
     index
